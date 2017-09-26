28d0b64b220f6abe25ae7327880db20312496ace
llcoolj84 <llcj84@gmail.com>
2017-09-26 17:16:53 -0600

    Fixed bug on search using knex

--- new_script_kinex.js
+++ new_script_kinex.js
@@ -1,50 +1,14 @@
-const settings = require("./settings"); // settings.json
 const name = process.argv[2];
 
-const client = new pg.Client({
-    user: settings.user,
-    password: settings.password,
-    database: settings.database,
-    host: settings.hostname,
-    port: settings.port,
-    ssl: settings.ssl
+const settings = require("./settings"); // settings.json
+var knex = require('knex')({
+    client: 'pg',
+    connection: {
+        user: 'development',
+        password: 'development'
+
+    }
 });
-
-// //connect to database
-// client.connect((err) => {
-//     console.log('node lookup_people.js Lincoln');
-//     if (err) {
-//         return console.error("Connection Error", err);
-//     }
-//     find_person();
-// });
-
-// // find person
-// function find_person(person) {
-
-//     console.log('Searching...');
-//     client.query('SELECT * FROM famous_people WHERE last_name = $1::text', name, (err, result) => {
-//         if (err) {
-//             return console.error("error running query", err);
-//         }
-//         print_result(result);
-//         client.end();
-//     });
-// }
-
-// //print output
-
-// function print_result(result) {
-
-//     result.rows.forEach(function(element) {
-//         console.log('Found ' + result.rows.length + " person(s)" + ': ' + element.first_name + ' ' +
-//             element.last_name + ', ' + 'born' + " '" + element.birthdate.toISOString().substr(0, 10) + "'");
-
-//     });
-// }
-
-//call find_person function
-find_person(name);
 
 //findPerson function
 findPerson(name);
@@ -58,9 +22,11 @@ function findPerson(lastname) {
 
 }
 
+// print search result to console
 function returnPerson(result) {
     result.forEach(function(element) {
         console.log('Found ' + result.length + " person(s)" + ': ' + element.first_name + ' ' +
             element.last_name + ', ' + 'born' + " '" + element.birthdate.toISOString().substr(0, 10) + "'");
 
     });
+}
